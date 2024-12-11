1fd1ebf9e618b824aa184dbb321808666412788c
omarcota12 <138240266+omarcota12@users.noreply.github.com>
2024-12-11 01:45:03 -0700

    Update Program.cs

--- Rawos_OCME/Program.cs
+++ Rawos_OCME/Program.cs
@@ -3,18 +3,12 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Registrar el servicio HttpClient para hacer solicitudes HTTP
 builder.Services.AddHttpClient();
 
-// Agregar controladores
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Configurar las rutas para los controladores
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dog}/{action=Home}/{id?}"); // Redirige a la acción 'Home' de DogController
-
-// Ejecutar la aplicación
+    pattern: "{controller=Dog}/{action=Home}/{id?}"); // Redirige a la acciÃ³n 'Home' de DogController
 app.Run();
